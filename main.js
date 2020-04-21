6f06c93c13e7735ec5e335dbf19575485e3949cc
QuiK000 <57329302+QuiK000@users.noreply.github.com>
2020-04-21 22:10:38 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,10 @@
-const video = document.querySelector('video');
-const btn = document.querySelector('.save');
-const canvas = document.querySelector('canvas');
-const photo = document.querySelector('#photo');
-
-
 if (navigator.webkitGetUserMedia !== null) {
   const options = {
     video: true,
     audio: true
   }
+
+  const video = document.querySelector('video');
 
   navigator.webkitGetUserMedia(options,
     (stream) => {
@@ -20,25 +16,3 @@ if (navigator.webkitGetUserMedia !== null) {
     }
   )
 }
-
-// function createDownloadLink(blob) {
-//   const url = URL.createObjectURL(blob);
-//   const filename = new Date().toISOString();
-
-//   const xhr = XMLHttpRequest();
-  
-//   xhr.onload((e) => {
-//     if (this.readyState === 4) {
-//       console.log("Server returned: ", e.target.responseText);
-//     }
-//   })
-
-//   const fd = new FormData();
-//   fd.append('audio_data', blob, filename);
-//   xhr.open('POST', 'upload.php', true);
-//   xhr.send(fd);
-
-//   window.setTimeout(() => {
-//     startRecording
-//   }, 300)
-// }
