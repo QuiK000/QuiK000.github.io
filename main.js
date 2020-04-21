3b5c48e6d078714b9bfb58102a3452f525ec9403
QuiK000 <57329302+QuiK000@users.noreply.github.com>
2020-04-21 22:42:09 +0300

    Update main.js

--- main.js
+++ main.js
@@ -2,21 +2,19 @@ const video = document.querySelector('video');
 const turnON = document.querySelector('.turn-on');
 const turnOFF = document.querySelector('.turn-off');
 
-var options = {
+const options = {
   video: true,
-  audio: false
+  audio: true
 }
 
 function ON() {
   if (navigator.webkitGetUserMedia !== null) {
-    options.audio.muted = true;
 
     navigator.webkitGetUserMedia(options,
       (stream) => {
         video.srcObject = stream;
         video.play();
-      },
-      (e) => {
+      },(e) => {
         console.error(`${e}`);
       }
     )
@@ -24,10 +22,12 @@ function ON() {
 }
 
 function OFF() {
-  options = {
-    video: false,
-    audio: false
-  };
+  navigator.webkitGetUserMedia(options, (stream) => {
+    video.srcObject = null;
+    video.stop();
+  },(e) => {
+    console.error(`${e}`);
+  })
 }
 
 turnON.addEventListener('click', ON);
